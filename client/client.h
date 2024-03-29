ae12aeddb52c6d4e8634cc28c23b1f3ed31884ad
Manuel R. Fungate Jr <manuelfungatejr@gmail.com>
2024-03-29 10:40:25 +0200

    HEADER

---
+++ client/client.h
@@ -0,0 +1,28 @@
+#ifndef
+#define
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+
+#define SERVER_PORT 80 /* http*/
+
+#define MAX_LINE 4096
+
+#define SA struct sockaddr
+
+void err_n_die(const char *format, ...);
+
+
+#endif
